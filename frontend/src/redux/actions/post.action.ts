682d53ee4568bd4db5780efab50322ed55a9c45d
Praveen Pandi <praveenpandi.alagar@aroopatech.com>
2025-03-29 17:21:50 +0530

    commited the env

--- frontend/src/redux/actions/post.action.ts
+++ frontend/src/redux/actions/post.action.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const API_URL = 'http://localhost:5000/api/posts';
+const API_URL =
+  import.meta.env.VITE_API_URL || 'http://localhost:5000/api/posts';
 
 // Fetch all posts
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
